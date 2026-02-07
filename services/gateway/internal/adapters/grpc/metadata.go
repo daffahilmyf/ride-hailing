8d5bc4d72f1b366bd4deaee985dca566ca1dd778
Daffa Hilmy Fadhlurrohman <daffahilmanafrizal@gmail.com>
2026-02-07 16:49:14 +0700

    feat(gateway): add grpc metadata helper

---
+++ services/gateway/internal/adapters/grpc/metadata.go
@@ -0,0 +1,15 @@
+package grpc
+
+import (
+	"context"
+
+	"google.golang.org/grpc/metadata"
+)
+
+func WithRequestMetadata(ctx context.Context, traceID, requestID string) context.Context {
+	md := metadata.Pairs(
+		"x-trace-id", traceID,
+		"x-request-id", requestID,
+	)
+	return metadata.NewOutgoingContext(ctx, md)
+}
