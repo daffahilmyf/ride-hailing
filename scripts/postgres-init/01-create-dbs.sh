23a9229c32236f5697f088d4e57892a6f03f93d4
Daffa Hilmy Fadhlurrohman <daffahilmanafrizal@gmail.com>
2026-02-08 18:00:02 +0700

    chore(compose): use shell init script

--- scripts/postgres-init/01-create-dbs.sh
+++ scripts/postgres-init/01-create-dbs.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env sh
+set -eu
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<'SQL'
 DO $$
 BEGIN
   IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'users') THEN
@@ -5,3 +9,4 @@ BEGIN
   END IF;
 END
 $$;
+SQL
